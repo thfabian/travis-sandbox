f34bf0b8b5e8c8735cb4a92011a7e9ec557ef38b
Fabian Thüring <fabian_thuering@hotmail.com>
2017-08-02 19:19:43 +0200

    trigger travis

--- prepare-cache.sh
+++ prepare-cache.sh
@@ -4,3 +4,6 @@ mkdir -p ${TRAVIS_BUILD_DIR}/cache-dir
 if [ ! -f ${TRAVIS_BUILD_DIR}/cache-dir/test.txt ]; then
   echo $(date) > ${TRAVIS_BUILD_DIR}/cache-dir/test.txt
 fi
+
+ls ${TRAVIS_BUILD_DIR}/cache-dir
+cat ${TRAVIS_BUILD_DIR}/cache-dir/test.txt
